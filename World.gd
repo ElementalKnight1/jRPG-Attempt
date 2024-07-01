14d6672681028f81e84f9325278901ffbae5c81d
Elemental Knight <elemental_knight1@yahoo.com>
2024-06-30 20:43:36 -0400

    Smoother World Map Movement
    Now you can hold down the input button to move!

--- World.gd
+++ World.gd
@@ -15,6 +15,7 @@ var inputs = {"move_right": Vector2.RIGHT,
 			"move_left": Vector2.LEFT,
 			"move_up": Vector2.UP,
 			"move_down": Vector2.DOWN}
+var pressed_direction = Vector2.ZERO
 
 const CharacterResource = preload("res://character.tscn")
 
@@ -66,33 +67,61 @@ func generate_map():
 			tile.set_edges()
 			
 func _unhandled_input(event):
-	for dir in inputs.keys():
-		if event.is_action_pressed(dir):
-			move(currentCharacter, dir)
-	if event.is_action_pressed("debug_refresh"):
-		await generate_map()
+	if event.is_action("move_up") or event.is_action("move_down"):
+		pressed_direction = Vector2.ZERO
+		pressed_direction.y = Input.get_axis("move_up","move_down")
+		if pressed_direction.y == 0.0:
+			pressed_direction.x = Input.get_axis("move_left","move_right")
+	elif event.is_action("move_left") or event.is_action("move_right"):
+		pressed_direction = Vector2.ZERO
+		pressed_direction.x = Input.get_axis("move_left","move_right")
+		if pressed_direction.x == 0.0:
+			pressed_direction.y = Input.get_axis("move_down","move_up")
+	#OLD INPUT
+	#for dir in inputs.keys():
+		#if event.is_action_pressed(dir):
+			#move(currentCharacter, dir)
+	#if event.is_action_pressed("debug_refresh"): #F5, of course
+		#await generate_map()
 
 func move(character, dir):
+	if character.isMoving or dir == Vector2.ZERO:
+		return #don't need to do anything
+		
 	var ray = character.get_node("RayCast2D")
-	#print(ray)
-	ray.target_position = inputs[dir] * (TileSize)
-	print(ray.target_position)
+	ray.target_position = dir * (TileSize)
 	ray.force_raycast_update()
-	if ray.is_colliding():
-		#print(ray.get_collider().get_parent())
-		#print(ray.get_collider())
-		print("Ray collision! No moving for you.")
-	else:
+	
+	if not ray.is_colliding():
 		var tween = create_tween()
 		tween.tween_property(character, "position",
-			character.position + inputs[dir] * TileSize, 1.0/3).set_trans(Tween.TRANS_SINE)
+			character.position + dir * TileSize, 1.0/4).set_trans(Tween.TRANS_LINEAR)
 		character.isMoving = true
 		await tween.finished
 		character.isMoving = false
+	#OLD INPUT
+	#var ray = character.get_node("RayCast2D")
+	##print(ray)
+	#ray.target_position = inputs[dir] * (TileSize)
+	#print(ray.target_position)
+	#ray.force_raycast_update()
+	#if ray.is_colliding():
+		##print(ray.get_collider().get_parent())
+		##print(ray.get_collider())
+		#print("Ray collision! No moving for you.")
+	#else:
+		#var tween = create_tween()
+		#tween.tween_property(character, "position",
+			#character.position + inputs[dir] * TileSize, 1.0/3).set_trans(Tween.TRANS_SINE)
+		#character.isMoving = true
+		#await tween.finished
+		#character.isMoving = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	move(currentCharacter,pressed_direction)
+	#OLD INPUT
+	#pass
 
 func generate_terrain_tile(x: int, y: int):
 	var tile = tile.instantiate()
